f4c1257ad50eef55ada60c54ce776f3452ba5894
wanguimbutu <92292540+wanguimbutu@users.noreply.github.com>
2023-06-20 14:22:34 +0300

    Add files via upload

---
+++ file handling/filestream.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+# include <fstream>
+
+using namespace std;
+
+int main(){
+   char input[75];
+   ofstream os;
+   os.open("testout.txt");
+   cout <<"Writing to text file"<<endl;
+   cout <<"Please enter your name: ";
+   cin.getline(input,100);
+   os << input << endl;
+   cout <<"Please enter your age: ";
+   cin >>input ;
+   cin.ignore();
+   os << input << endl;
+   os.close();
+   ifstream is;
+   string line;
+   is.open("testout.txt");
+   cout <<" Reading from text file" << endl;
+   while (getline(is,line))
+   {
+    cout << line << endl;
+   }
+   is.close();
+   return 0;
+   
+    
+}
